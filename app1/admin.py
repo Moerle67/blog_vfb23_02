38c22cb5ddab7c763edeb7a9309ac30b8666c21d
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2024-02-03 13:58:44 +0100

    Templates

--- app1/admin.py
+++ app1/admin.py
@@ -4,4 +4,10 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Blog)
+# admin.site.register(Blog)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['header', 'author', 'written', 'changed']
+    list_filter = ['author', 'rating']
+    search_fields = ['header', 'contents']
